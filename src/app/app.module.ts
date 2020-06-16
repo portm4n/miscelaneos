d32dee7ca906c2e51f2644ba8d03e374ad7d34e2
Portm4n <ruben_sanfer_3@hotmail.com>
2020-06-16 13:51:32 +0200

    v

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import { ClasesComponent } from './components/clases/clases.component';
 import { ResaltadoDirective } from './directivas/resaltado.directive';
 import { NsSwitchComponent } from './components/ns-switch/ns-switch.component';
 import { HomeComponent } from './components/home/home.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
+import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
+import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +26,11 @@ import { HomeComponent } from './components/home/home.component';
     ResaltadoDirective,
     NsSwitchComponent,
     HomeComponent,
+    UsuarioComponent,
+    UsuarioNuevoComponent,
+    UsuarioEditarComponent,
+    UsuarioDetalleComponent,
+    NavbarComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, APP_ROUTING],
   providers: [],
